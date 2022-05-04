ad564e0bf98d662f73bac57661e8aa3a9c6f123c
minsoo cho <khg04170@gmail.com>
2022-05-04 18:58:28 +0100

    added detail component

--- src/app.jsx
+++ src/app.jsx
@@ -6,10 +6,12 @@ import All from './components/all/all';
 import Detail from './components/detail/detail';
 import Login from './components/login/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 // 1. 페이지는 3개, 전체, 디테일, 로그인페이지.
 // 2. 유저는 전체 페이지를 볼 수 있다.
 //3. 유저는 상품 클릭 시 로그인이 되어있으면 상품 디테일 페이지로 넘어가 디테일을 볼 수있다.
-//4. 로그인이 되어있지않으면 로그인 페이지로 간다
+// 유저는 검색할 수 있다. (검색창만들기 + 검색 쿼리 url 요청 )
+//4. 유저는 상품 클릭 시  로그인이 되어있지않으면 로그인 페이지로 간다
 //5. 로그인 버튼 클릭시 로그인페이지로 간다.
 //6 . 로그인이 되어있으면 네비게이션 바에 로그아웃, 로그아웃 상태면 로그인이라고 표시된다.
 
--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import styles from './detail.module.css';
 
 const Detail = (props) => {
 	let { id } = useParams();
 	const [product, setProduct] = useState([]);
-	const getDetail = () => {
+	const getDetail = async () => {
 		let url = `https://my-json-server.typicode.com/minsoocho-hj/react-hnm2/products/${id}`;
+		let res = await fetch(url);
+		let data = await res.json();
+		console.log(data);
+		setProduct(data);
 	};
+
+	useEffect(() => {
+		getDetail();
+	}, []);
 	return (
 		<Container>
 			<Row>
 				<Col lg={6} md={5} sm={12}>
-					<img src='' alt='' className='img' />
+					<img src={product.img} alt='' className={styles.img} />
 				</Col>
 				<Col lg={6} md={5} sm={12}>
-					<div className='desc'>
-						<p className='title'>title</p>
-						<p className='price'>price</p>
+					<div className={styles.desc}>
+						<p className={styles.title}>{product.title}</p>
+						<p className={styles.price}>{product.price}</p>
+						<p className={styles.choice}>
+							{product.choice ? 'Conscious choice' : ''}
+						</p>
+						<p className={styles.new}>{product.new ? 'new' : ''}</p>
+						<button className={styles.btn}>Add</button>
 					</div>
 				</Col>
 			</Row>
